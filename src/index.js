1a711e7b4d2c74ccb90e773952c5015db80c967b
AAGrushevsky <111446919+AAGrushevsky@users.noreply.github.com>
2025-09-06 19:27:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,105 +1,66 @@
+const numbersZeroToNine = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+};
+
+const numbersElevenToNineteen = {
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+};
+
+const numbersTens = {
+  10: 'ten',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+};
+
+const zeroes = {
+  2: 'hundred',
+  3: 'thousand',
+  6: 'million',
+  9: 'billion',
+};
+
 module.exports = function toReadable(number) {
-    let result = '';
+  const zeroCounts = [...number.toString()].length - 1;
 
-    if (number === 0) {
-        result = 'zero';
-    }
+  if (zeroCounts === 0) {
+    return numbersZeroToNine[number];
+  }
+  if (zeroCounts === 1 && number % 10 === 0) {
+    return numbersTens[number];
+  }
+  if (zeroCounts === 1 && number % 10 !== 0 && number < 20) {
+    return numbersElevenToNineteen[number];
+  }
+  if (zeroCounts === 1 && number % 10 !== 0 && number > 20) {
+    return `${numbersTens[number - (number % 10)]} ${toReadable(number % 10)}`;
+  }
 
-    if (number >= 100) {
-        result = toReadable(Math.floor(number / 100)) + ' hundred' + (number % 100 == 0 ? '' : ' ' + toReadable(number % 100));
-    } else if (number < 20) {
-        switch (number) {
-            case 1:
-                result += 'one';
-                break;
-            case 2:
-                result += 'two';
-                break;
-            case 3:
-                result += 'three';
-                break;
-            case 4:
-                result += 'four';
-                break;
-            case 5:
-                result += 'five';
-                break;
-            case 6:
-                result += 'six';
-                break;
-            case 7:
-                result += 'seven';
-                break;
-            case 8:
-                result += 'eight';
-                break;
-            case 9:
-                result += 'nine';
-                break;
-            case 10:
-                result += 'ten';
-                break;
-            case 11:
-                result += 'eleven';
-                break;
-            case 12:
-                result += 'twelve';
-                break;
-            case 13:
-                result += 'thirteen';
-                break;
-            case 14:
-                result += 'fourteen';
-                break;
-            case 15:
-                result += 'fifteen';
-                break;
-            case 16:
-                result += 'sixteen';
-                break;
-            case 17:
-                result += 'seventeen';
-                break;
-            case 18:
-                result += 'eighteen';
-                break;
-            case 19:
-                result += 'nineteen';
-                break;
-        }
-    } else {
-        if (number % 10 === 0) {
-            switch (number) {
-                case 20:
-                    result += 'twenty';
-                    break;
-                case 30:
-                    result += 'thirty';
-                    break;
-                case 40:
-                    result += 'forty';
-                    break;
-                case 50:
-                    result += 'fifty';
-                    break;
-                case 60:
-                    result += 'sixty';
-                    break;
-                case 70:
-                    result += 'seventy';
-                    break;
-                case 80:
-                    result += 'eighty';
-                    break;
-                case 90:
-                    result += 'ninety';
-                    break;
-            }
-        } else {
-            let reminder = number % 10;
-            result += toReadable(number - reminder) + ' ' + toReadable(reminder);
-        }
-    }
+  if (zeroCounts === 2 && number % 100 === 0) {
+    return `${numbersZeroToNine[Math.floor(number / 10 ** zeroCounts)]} ${zeroes[zeroCounts]}`;
+  }
 
-    return result;
-}
+  return `${numbersZeroToNine[Math.floor(number / 10 ** zeroCounts)]} ${zeroes[zeroCounts]} ${toReadable(number % 10 ** zeroCounts)}`.trim();
+};
